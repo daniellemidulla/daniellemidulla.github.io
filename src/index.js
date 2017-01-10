8e89f65eb481bfc2b23ac4a4e8b6e1625b2b2e93
Danielle Midulla <danielle.midulla@gmail.com>
2017-01-09 20:46:53 -0500

    Adding functionality to the navigation bar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 // index.js
 
+import $ from 'jquery';
+
 // add the style sheet onto the page
 import './style.scss';
+
+$("#nav-home").click((e) => {
+  e.preventDefault();
+  $(document).scrollTop(0);
+});
+
+$("#nav-about").click((e) => {
+  e.preventDefault();
+  let aboutMe = $("#about-me");
+  let offset = aboutMe.offset();
+  $(document).scrollTop(offset.top - 50);
+});
+
+$("#nav-skills").click((e) => {
+  e.preventDefault();
+  let skills = $("#skills");
+  let offset = skills.offset();
+  $(document).scrollTop(offset.top - 50);
+});
+
+$("#nav-contact").click((e) => {
+  e.preventDefault();
+  let contactMe = $("#contact-me");
+  let offset = contactMe.offset();
+  $(document).scrollTop(offset.top - 50);
+});
